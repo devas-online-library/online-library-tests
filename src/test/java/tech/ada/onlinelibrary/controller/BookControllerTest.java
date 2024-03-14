749f1a4dd7db9437858e3136423421536486022b
anajlm <anaj.limartins@gmail.com>
2024-03-13 22:22:52 -0300

    fix: book controller test

--- src/test/java/tech/ada/onlinelibrary/controller/BookControllerTest.java
+++ src/test/java/tech/ada/onlinelibrary/controller/BookControllerTest.java
@@ -16,11 +16,14 @@ import tech.ada.onlinelibrary.service.BookService;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.time.Year;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -31,7 +34,7 @@ import org.mockito.Mockito;
 class BookControllerTest {
 
     private Book book;
-    private List<Book> books;
+    private List<Book> books = new ArrayList<>();
 
     private MockMvc mockMvc;
 
@@ -44,6 +47,7 @@ class BookControllerTest {
     @BeforeEach
     public void setup() {
         book = new Book(1l, "Clean Code: A Handbook of Agile Software Craftsmanship", "Robert C. Martin", Genre.TECHNICAL, "Prentice Hall", Year.of(2008));
+        books.add(book);
         mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
     }
 
@@ -62,9 +66,11 @@ class BookControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/library/books")
                         .contentType(MediaType.APPLICATION_JSON))
-                        .andExpect(status().isOk());
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.[0].id", equalTo(1)));
 
-        verify(bookService,times(1)).getAllBooks();
+        //verify(bookService,times(1)).getAllBooks();
+
     }
 
 
