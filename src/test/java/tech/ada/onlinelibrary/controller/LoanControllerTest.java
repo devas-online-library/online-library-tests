a7ab57009e10ee7c04c20d9af492671d0117a75a
anajlm <anaj.limartins@gmail.com>
2024-03-17 15:03:27 -0300

    add: LoanControllerTest

---
+++ src/test/java/tech/ada/onlinelibrary/controller/LoanControllerTest.java
@@ -0,0 +1,32 @@
+package tech.ada.onlinelibrary.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanControllerTest {
+
+    @Test
+    void newLoan() {
+    }
+
+    @Test
+    void getAllLoans() {
+    }
+
+    @Test
+    void getLoansByUserId() {
+    }
+
+    @Test
+    void registerLoanReturn() {
+    }
+
+    @Test
+    void renewLoan() {
+    }
+
+    @Test
+    void deleteLoan() {
+    }
+}
---
+++ src/test/java/tech/ada/onlinelibrary/service/BookServiceTest.java
@@ -0,0 +1,35 @@
+package tech.ada.onlinelibrary.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookServiceTest {
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void getBooksByTitle() {
+    }
+
+    @Test
+    void getBooksByAuthor() {
+    }
+
+    @Test
+    void getBooksByGenre() {
+    }
+
+    @Test
+    void createBook() {
+    }
+
+    @Test
+    void getBookById() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+}
